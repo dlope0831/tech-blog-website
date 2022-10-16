f9cc0f8832d462997d8cac851901db05d323b111
Daniela Lopez <dlope0831@gmail.com>
2022-10-15 20:02:07 -0500

    updating file

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
-
-const routes = require('./controllers');
+// const routes = require('./routes');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(require('./controllers'));
 
-app.use(routes);
+// app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
 });
+
